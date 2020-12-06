631c030f2378372bbda958fe5c48c98835d669ec
Gebrayel <gebrayelinatti06@gmail.com>
2020-12-06 16:29:35 -0400

    agregar input menu

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from funciones import*
 from prettytable import PrettyTable
 
 agregarPelicula()
+while menu:
+    decision = input("Welcome to Blockbuster's System! \n\n  If you wanna ADD A NEW MOVIE, type 1\n\n  If you wanna DELETE A MOVIE, type 2\n\n  If you wanna CHECK IF A MOVIE IS RENTED, type 3\n\n  If you wanna RETURN A MOVIE, type 4\n\n  Type 5 to PACK\n\n")
+    
+
