36f7e8ac233bbd0fd3ea7f068bd247bba41e109b
Gebrayel <gebrayelinatti06@gmail.com>
2020-12-07 14:27:19 -0400

    arreglar merge details

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ print(peliculas)
 
 menu = True
 while menu:
-    decision = input("Welcome to Blockbuster's System! \n\n  If you wanna ADD A NEW MOVIE, type 1\n\n  If you wanna DELETE A MOVIE, type 2\n\n  If you wanna CHECK IF A MOVIE IS RENTED, type 3\n\n  If you wanna RETURN A MOVIE, type 4\n\n If you wanna RENT A MOVIE, type 5\n\n  Type 6 to PACK\n\n  Type any other thing to EXIT\n\n")
+    decision = input("Welcome to Blockbuster's System! \n\n  If you wanna ADD A NEW MOVIE, type 1\n\n  If you wanna DELETE A MOVIE, type 2\n\n  If you wanna CHECK IF A MOVIE IS RENTED, type 3\n\n  If you wanna RETURN A MOVIE, type 4\n\n  If you wanna RENT A MOVIE, type 5\n\n  Type 6 to PACK\n\n  Type any other thing to EXIT\n\n")
     
     if decision == "1":
         agregarPelicula()
@@ -59,7 +59,7 @@ while menu:
         print("\n\nPACKING SUCCESSFUL!\n\n")
     else:
         menu = False
-    sleep(6)
+    sleep(3)
 
 # seguir = 1
 # while seguir == 1:
